32655c7e46777e4257a079d7426277684116b064
moseskim <creatinov.kim@gmail.com>
2025-01-04 10:27:55 +0900

    Add files via upload

--- app/api/item/readall/route.js
+++ app/api/item/readall/route.js
@@ -6,8 +6,10 @@ export async function GET() {
   try {
     await connectDB()
     const allItems = await ItemModel.find()  // 추가
-    return NextResponse.json({message: "아이템 읽기 성공(전체)", allItems: allItems })
+    return NextResponse.json({message: "아이템 읽기 성공(모두)", allItems: allItems })
   } catch {
-    return NextResponse.json({message: "아이템 읽기 실패(전체)"})
+    return NextResponse.json({message: "아이템 읽기 실패(모두)"})
   }
 }
+
+export const revalidate = 0
